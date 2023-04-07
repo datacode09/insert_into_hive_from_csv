b5908dad8976f17747293d440e453f6f63130e97
Abul Fahad <117757435+datacode09@users.noreply.github.com>
2023-04-07 18:46:28 -0400

    Create create_2.sh

---
+++ create_2.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Set the name of the database
+database="dev"
+
+# Set the name of the table
+table="my_table"
+
+# Set the location of the table data
+location="/path/to/table/data"
+
+# Set the name of the partition column
+partition_column="datetime"
+
+# Set the partition format (YYYY-MM-DD)
+partition_format="%Y-%m-%d"
+
+# Set the path to the CSV file on the edge node
+csv_file="/path/to/csv/file.csv"
+
+# Set the delimiter used in the CSV file
+delimiter=","
+
+# Set the number of mappers to use for parallel processing
+num_mappers=4
+
+# Set the DDL statement for the table
+ddl=$(cat <<EOF
+CREATE TABLE ${database}.${table} (
+  id INT,
+  name STRING,
+  age INT,
+  email STRING,
+  address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>,
+  salary DOUBLE,
+  ${partition_column} TIMESTAMP
+)
+PARTITIONED BY (${partition_column} STRING)
+STORED AS ORC
+LOCATION '${location}';
+EOF
+)
+
+# Execute the Hive command to create the table
+hive -e "${ddl}"
+
+# Load data into the table
+load_data_ddl=$(cat <<EOF
+SET mapred.map.tasks=${num_mappers};
+INSERT INTO ${database}.${table} PARTITION (${partition_column})
+SELECT
+  id,
+  name,
+  age,
+  email,
+  address,
+  salary,
+  CAST(FROM_UNIXTIME(UNIX_TIMESTAMP(${partition_column}, '${partition_format}')) AS TIMESTAMP) AS ${partition_column}
+FROM
+  (
+    SELECT
+      TRANSFORM (split(line, '${delimiter}'))
+      USING 'python /path/to/script.py'
+      AS (id INT, name STRING, age INT, email STRING, address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>, salary DOUBLE, ${partition_column} STRING)
+    FROM
+      (SELECT get_file_content('${csv_file}') AS line) t1
+  ) t2;
+EOF
+)
+
+# Execute the Hive command to load the data
+hive -e "${load_data_ddl}"
